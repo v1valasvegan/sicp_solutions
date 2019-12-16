115f9c2bcc54cc4151e08f9567995493a98fbc3c
Victor Zhuravlev <mocambe@mail.ru>
2019-12-16 14:43:09 +0300

    1.3, 1.5, 1.6

---
+++ sicp/chapter1/1.6.rkt
@@ -0,0 +1,21 @@
+#lang racket/base
+(require "math.rkt")
+
+(define (good-enough? guess x) 
+    (< (abs (- (square guess) x)) 0.0001))
+    
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x)
+                    x)))
+
+(define (sqrt x)
+    (sqrt-iter 1.0 x))
+
+
+
+(displayln (sqrt 2))
--- sicp/chapter1/math.rkt
+++ sicp/chapter1/math.rkt
@@ -5,3 +5,9 @@
 (define (square x) (* x x))
 
 (define (sum-of-squares x y) (+ (square x) (square y)))
+
+(define (abs x)
+    (if (> x 0) x (- x)))
+
+(define (average x y)
+    (/ (+ x y) 2))
